94ebf1c6029cf2c3ee918b507037d249fa6acae3
xaviertrepat <61465699+xaviertrepat@users.noreply.github.com>
2020-02-25 16:11:09 +0100

    master creation
    master creation

---
+++ src/main/java/com/privalia/publicador_kafka/PublicadorKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.privalia.publicador_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicadorKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicadorKafkaApplication.class, args);
+    }
+
+}
